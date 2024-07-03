015258865af0140ffc3d4cd3257220c75ecb6c4d
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-07-03 19:32:39 +0800

    first commit

---
+++ day1/second.kt
@@ -0,0 +1,7 @@
+fun addInt(a: Int, b: Int) : Int{
+  return a + b
+}
+
+fun main(){
+  println(addInt(1,2))
+}
---
+++ day2/main.kt
@@ -0,0 +1,33 @@
+package io.pivotal
+import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpHandler
+import com.sun.net.httpserver.HttpServer
+import java.net.InetSocketAddress
+
+fun main(args: Array<String>) {
+    val server = HttpServer.create(InetSocketAddress(8000), 0)
+    server.createContext("/test", MyHandler())
+    server.createContext("/", IndexHandler())
+    server.executor = null // creates a default executor
+    server.start()
+}
+
+class MyHandler : HttpHandler {
+    override fun handle(t: HttpExchange) {
+        val response = "This is the response"
+        t.sendResponseHeaders(200, response.length.toLong())
+        val os = t.responseBody
+        os.write(response.toByteArray())
+        os.close()
+    }
+}
+
+class IndexHandler : HttpHandler{
+  override fun handle(t: HttpExchange){
+    val response = "Hello world"
+    t.sendResponseHeaders(200, response.length.toLong())
+    val os = t.responseBody
+    os.write(response.toByteArray())
+    os.close()
+  }
+}
