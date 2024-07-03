25ab2b3bb8ac0188cbefeeaa4f94dba024a570a7
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-07-03 19:34:43 +0800

    changed main method to use no arguements in main function

--- day2/main.kt
+++ day2/main.kt
@@ -4,7 +4,7 @@ import com.sun.net.httpserver.HttpHandler
 import com.sun.net.httpserver.HttpServer
 import java.net.InetSocketAddress
 
-fun main(args: Array<String>) {
+fun main() {
     val server = HttpServer.create(InetSocketAddress(8000), 0)
     server.createContext("/test", MyHandler())
     server.createContext("/", IndexHandler())
