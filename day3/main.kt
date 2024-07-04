1fb319084f21f7005805b82ff31086ee45c4c1dc
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-07-05 00:29:37 +0800

    day3 completed.

--- day3/main.kt
+++ day3/main.kt
@@ -1,16 +1,40 @@
 import java.lang.System
 import java.util.Scanner
-fun main(){
-    val reader = Scanner(java.lang.System.`in`);
+
+fun main() {
+    var reader = Scanner(java.lang.System.`in`)
     print("Enter your age this year > ")
-    Byte age = reader.nextByte();
+    var age = reader.nextByte()
     println("------------------------------------")
-    print("Enter your salary per month in SGD > ");
-    Int salary = reader.nextInt();
+    print("Enter your salary per month in SGD > ")
+    var salary = reader.nextInt()
     println("------------------------------------")
-    if(age >= 18 && age < 55){
-        Int remainingSalary = salary * 0.80;
-        Float CPF = salary * 0.37;
-        println("Your salary would be $remainingSalary and your CPF will be $CPF");
+    if (age < 55) {
+        var remainingSalary = salary * 0.80
+        var CPF = salary * 0.370
+        println("Your remaining salary would be $remainingSalary and your CPF will be $CPF")
+    }
+    else if (age >= 55 && age < 60) {
+        var remainingSalary = salary * 0.83
+        var CPF = salary * 0.310
+        println("Your remaining salary would be $remainingSalary and your CPF will be $CPF")
+    }
+
+    else if (age >= 60 && age < 65) {
+        var remainingSalary = salary * 0.885
+        var CPF = salary * 0.220
+        println("Your remaining salary would be $remainingSalary and your CPF will be $CPF")
+    }
+
+    else if (age >= 65 && age < 70) {
+        var remainingSalary = salary * 0.075
+        var CPF = salary * 0.165
+        println("Your remaining salary would be $remainingSalary and your CPF will be $CPF")
+    }
+
+    else if (age > 70) {
+        var remainingSalary = salary * 0.05
+        var CPF = salary * 0.125
+        println("Your remaining salary would be $remainingSalary and your CPF will be $CPF")
     }
 }
